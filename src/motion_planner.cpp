f6589c7b0e8fe93b486dd18fef26bcde247cf5d4
Solomon Wiznitzer <shlomo.wiz@gmail.com>
2019-03-25 23:51:07 -0500

    should've chose a smaller package name. anyway, the class skeleton for the motion planner is implemented. still have to fill in some function code, but it builds and runs!

--- src/motion_planner.cpp
+++ src/motion_planner.cpp
@@ -1,12 +1,50 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include <geometry_msgs/Point.h>
+#include <iostream>
+#include <vector>
+#include "multi_agent_planner/get_plan.h"
+#include "multi_agent_planner/agent_info.h"
 
-int main( int argc, char** argv )
+class Motion_Planner
 {
-    ros::init(argc, argv, "nodes");
-    ros::NodeHandle n;
-    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+public:
+    Motion_Planner(ros::NodeHandle *node_handle);
+private:
+    ros::NodeHandle node;
+    ros::Publisher pub_marker;
+    ros::Subscriber sub_agent_pose;
+    ros::ServiceServer srv_get_plan;
+    void create_roadmap();
+    bool get_plan(multi_agent_planner::get_plan::Request &req, multi_agent_planner::get_plan::Response &res);
+    void agent_start_pose_callback(const multi_agent_planner::agent_info &msg);
+    std::vector<geometry_msgs::Point> path_list;
+
+
+};
+
+Motion_Planner::Motion_Planner(ros::NodeHandle *node_handle)
+    : node(*node_handle)
+{
+    pub_marker = node.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+    sub_agent_pose = node.subscribe("/agent_feedback", 100, &Motion_Planner::agent_start_pose_callback, this);
+    srv_get_plan = node.advertiseService("/get_plan", &Motion_Planner::get_plan, this);
+
+    create_roadmap();
+}
+
+bool Motion_Planner::get_plan(multi_agent_planner::get_plan::Request &req, multi_agent_planner::get_plan::Response &res)
+{
+    return true;
+}
+
+void Motion_Planner::agent_start_pose_callback(const multi_agent_planner::agent_info &msg)
+{
+    ;
+}
+
+void Motion_Planner::create_roadmap()
+{
     visualization_msgs::Marker marker;
     marker.header.frame_id = "/world";
     marker.header.stamp = ros::Time();
@@ -32,14 +70,23 @@ int main( int argc, char** argv )
             p.y = j;
             marker.points.push_back(p);
         }
-    while (marker_pub.getNumSubscribers() < 1)
+    while (pub_marker.getNumSubscribers() < 1)
      {
        if (!ros::ok())
        {
-         return 0;
+         break;
        }
        ROS_WARN_ONCE("Please create a subscriber to the marker");
        sleep(1);
      }
-    marker_pub.publish(marker);
+    pub_marker.publish(marker);
+}
+
+int main( int argc, char** argv )
+{
+    ros::init(argc, argv, "nodes");
+    ros::NodeHandle n;
+    Motion_Planner mp(&n);
+    ros::spin();
+    return 0;
 }
